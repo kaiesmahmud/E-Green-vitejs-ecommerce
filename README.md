5dab56a3783cfe023a9ada9f851e97709342ec18
kaiesmahmud <kaiesmahmud@gmail.com>
2023-04-09 09:29:21 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# E-Green-vitejs-ecommerce
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='mt-20 text-center text-xl'>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
